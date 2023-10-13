71469a082a7b07a781a9ba7b65b8e202bd235243
elitucker42 <62191340+elitucker42@users.noreply.github.com>
2023-10-12 23:02:37 -0400

    choose date and read words from csv

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-const gameBoard = document.getElementById('game-board');
-const rows = 4;
-const cols = 4;
-let board = Array.from(Array(rows), () => new Array(cols).fill(null));
+let data;
+let board;
 
-function createBoard() {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            let cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.dataset.row = i;
-            cell.dataset.col = j;
-            cell.addEventListener('click', handleCellClick);
-            gameBoard.appendChild(cell);
+// Load and parse the CSV file
+async function loadCSV() {
+    const response = await fetch('data.csv');
+    const csvText = await response.text();
+    data = Papa.parse(csvText, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            populateDateSelector(results.data);
         }
+    });
+}
+
+// Populate the date selector dropdown
+function populateDateSelector(rows) {
+    const dateSelector = document.getElementById('date-selector');
+    const dates = [...new Set(rows.map(row => row.date))];  // Get unique dates
+    dates.forEach(date => {
+        const option = document.createElement('option');
+        option.value = date;
+        option.textContent = date;
+        dateSelector.appendChild(option);
+    });
+}
+
+// Handle date selection
+function handleDateChange(event) {
+    const selectedDate = event.target.value;
+    const wordsForDate = data.data.filter(row => row.date === selectedDate);
+    populateBoard(wordsForDate);
+    document.getElementById('game-board').style.display = 'block';  // Show the game board
+}
+
+// Shuffle array utility function
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];  // Swap elements
     }
 }
 
-function handleCellClick(event) {
-    let row = event.target.dataset.row;
-    let col = event.target.dataset.col;
-    board[row][col] = 'X';  // For simplicity, just marking cells with 'X' on click
-    renderBoard();
+// Populate the game board with words and colors
+function populateBoard(wordsForDate) {
+    const gameBoard = document.getElementById('game-board');
+    shuffle(wordsForDate);  // Randomize word order
+    wordsForDate.forEach((wordObj, index) => {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.style.backgroundColor = wordObj.color;
+        cell.textContent = wordObj.word;
+        gameBoard.appendChild(cell);
+    });
 }
 
-function renderBoard() {
-    let cells = gameBoard.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-        let row = cells[i].dataset.row;
-        let col = cells[i].dataset.col;
-        cells[i].textContent = board[row][col];
-    }
-}
-
-createBoard();
+// Initial call to load CSV and populate date selector
+loadCSV();
